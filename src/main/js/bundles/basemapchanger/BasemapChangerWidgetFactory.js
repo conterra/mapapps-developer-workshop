4cad5de2fc2823133bfa925f1cf2c98bb168cdec
Matthias Stein <m.stein@conterra.de>
2021-11-16 08:44:53 +0100

    Fix binding

--- src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
+++ src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
@@ -40,8 +40,8 @@ export default class BasemapChangerWidgetFactory {
 
         this.#binding = Binding.for(vm, basemapsModel)
             .syncAll("selectedId")
-            .syncToLeftNow()
-            .enable();
+            .enable()
+            .syncToLeftNow();
     }
 
 }
