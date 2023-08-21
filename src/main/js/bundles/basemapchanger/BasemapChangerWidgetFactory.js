c66df44312e1b9cec1563384c3c6541b180694de
cherry13579 <lia.kirsch@googlemail.com>
2023-08-21 13:32:54 +0200

    Übung 3 Schritt 5

--- src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
+++ src/main/js/bundles/basemapchanger/BasemapChangerWidgetFactory.js
@@ -20,8 +20,17 @@ import BasemapChangerWidget from "./BasemapChangerWidget.vue";
 export default class BasemapChangerWidgetFactory {
 
     createInstance() {
+        const basemapsModel = this._basemapsModel;
+        const basemaps = basemapsModel.basemaps.map((basemap) => {
+            return {
+                id: basemap.id,
+                title: basemap.title
+            };
+        });
 
         const vm = new Vue(BasemapChangerWidget);
+        vm.basemaps = basemaps;
+        vm.selectedId = basemapsModel.selectedId;
         return VueDijit(vm, {class: "basemapchanger-widget"});
 
     }
